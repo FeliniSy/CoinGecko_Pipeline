81893a40252c865a347b250c3c42d57880843654
FeliniSy <ninikheladze9@gmail.com>
2026-04-07 17:12:25 +0400

    refactor: move data fetching from GCS to transformation module

--- pipeline/backfill_pipeline/load_backfill.py
+++ pipeline/backfill_pipeline/load_backfill.py
@@ -1,58 +1,17 @@
-import json
-
-from google.cloud import storage, bigquery
+from google.cloud import bigquery
 
 from gcp.bigquery.schema import json_schema
 from pipeline.backfill_pipeline.transform_backfill import transform_historical_data
 from utils.logger import log
-from utils.settings import GCS_BUCKET, PROJECT_ID, BQ_DATASET, BQ_STAGING
+from utils.settings import PROJECT_ID, BQ_DATASET, BQ_STAGING
 
 
 def load_backfill_to_staging(**context) -> int:
+    log.info("Starting backfill transform and load to staging...")
 
-    manifest_path: str = context["ti"].xcom_pull(task_ids="fetch_historical")
+    rows = transform_historical_data(**context)
 
-    log.info("Loading backfill_pipeline manifest from: gs://%s/%s", GCS_BUCKET, manifest_path)
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(GCS_BUCKET)
-    manifest_blob = bucket.blob(manifest_path)
-    manifest = json.loads(manifest_blob.download_as_text())
-
-    coin_files = manifest["coin_files"]
-    total_coins = manifest["total_coins"]
-    successful_coins = manifest["successful_coins"]
-    failed_coins = manifest["failed_coins"]
-
-    log.info("Backfill manifest: %s coin files, %s/%s coins successful",
-             len(coin_files), successful_coins, total_coins)
-
-    if failed_coins:
-        log.warning("Failed coins (%s): %s", len(failed_coins), failed_coins[:10])
-
-    all_historical_data = []
-
-    for idx, coin_file in enumerate(coin_files, 1):
-        log.info("Reading coin %s/%s from gs://%s/%s",
-                 idx, len(coin_files), GCS_BUCKET, coin_file)
-
-        coin_blob = bucket.blob(coin_file)
-        coin_data = json.loads(coin_blob.download_as_text())
-
-        coin_data["source_file"] = f"gs://{GCS_BUCKET}/{coin_file}"
-        all_historical_data.append(coin_data)
-
-        log.info("Coin %s: loaded %s price points (total coins so far: %s)",
-                 idx, len(coin_data.get("prices", [])), len(all_historical_data))
-
-    if not all_historical_data:
-        raise ValueError("No historical data found in batches")
-
-    log.info("Total historical data loaded: %s coins", len(all_historical_data))
-
-    rows = transform_historical_data(all_historical_data)
-
-    log.info("Transformed %s coins into %s rows", len(all_historical_data), len(rows))
+    log.info("Transformed data into %s rows", len(rows))
 
     bq = bigquery.Client(project=PROJECT_ID)
     table_ref = f"{PROJECT_ID}.{BQ_DATASET}.{BQ_STAGING}"
