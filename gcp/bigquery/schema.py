ff4c8a90af45047210a8e545c02fb82039b5adf6
FeliniSy <ninikheladze9@gmail.com>
2026-04-07 17:13:33 +0400

    ref: format file

--- gcp/bigquery/schema.py
+++ gcp/bigquery/schema.py
@@ -5,7 +5,7 @@ json_schema = [
     bigquery.SchemaField("source_file", "STRING"),
     bigquery.SchemaField("ingested_at", "TIMESTAMP", mode='REQUIRED')
 ]
-# Schema for crypto_prices_staging and crypto_prices tables
+
 schema = [
     bigquery.SchemaField("coin_id", "STRING", mode="REQUIRED"),
     bigquery.SchemaField("symbol", "STRING"),
@@ -13,7 +13,6 @@ schema = [
     bigquery.SchemaField("price", "FLOAT64", mode="REQUIRED"),
     bigquery.SchemaField("market_cap", "FLOAT64"),
     bigquery.SchemaField("volume", "FLOAT64"),
-    bigquery.SchemaField("timestamp", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("ingested_at", "STRING"),
+    bigquery.SchemaField("timestamp", "TIMESTAMP", mode="REQUIRED"),
     bigquery.SchemaField("source_file", "STRING"),
 ]
