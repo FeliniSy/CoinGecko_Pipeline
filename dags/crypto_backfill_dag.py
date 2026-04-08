f6ed0a0e865f1f722818669b7a02579a91bc6414
FeliniSy <ninikheladze9@gmail.com>
2026-04-08 19:59:38 +0400

    ref: crypto_backfill_dag.py and crypto_ingestion_dag.py

--- dags/crypto_backfill_dag.py
+++ dags/crypto_backfill_dag.py
@@ -3,9 +3,9 @@ from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from pipeline.backfill_pipeline.extract_backfill import get_coin_list, fetch_historical_data
+from pipeline.backfill_pipeline.extract_backfill import fetch_historical_data
 from pipeline.backfill_pipeline.load_backfill import load_backfill_to_staging
-from pipeline.merge import upsert_to_final
+from pipeline.merge import upsert_backfill_to_final
 from utils.logger import log
 
 default_args = {
@@ -24,37 +24,28 @@ with DAG(
         dag_id="crypto_backfill_dag",
         default_args=default_args,
         description="Backfill 90 days of historical crypto data from CoinGecko → GCS → BigQuery",
-        schedule_interval=None,
+        schedule_interval=None,  # Manual trigger only
         start_date=datetime(2025, 1, 1),
         catchup=False,
         max_active_runs=1,
-        tags=["crypto", "backfill_pipeline", "historical"],
+        tags=["crypto", "backfill", "historical"],
 ) as dag:
-    t1 = PythonOperator(
-        task_id="get_coin_list",
-        python_callable=get_coin_list,
-
-    )
-
-    t2 = PythonOperator(
+    extract = PythonOperator(
         task_id="fetch_historical",
         python_callable=fetch_historical_data,
         execution_timeout=timedelta(hours=3),
-
     )
 
-    t3 = PythonOperator(
+    transform_load = PythonOperator(
         task_id="load_to_staging",
         python_callable=load_backfill_to_staging,
         execution_timeout=timedelta(minutes=30),
-
     )
 
-    t4 = PythonOperator(
+    upsert = PythonOperator(
         task_id="upsert_to_final",
-        python_callable=upsert_to_final,
-        execution_timeout=timedelta(minutes=20),
-
+        python_callable=upsert_backfill_to_final,
+        execution_timeout=timedelta(hours=1),
     )
 
-    t1 >> t2 >> t3 >> t4
+    extract >> transform_load >> upsert
