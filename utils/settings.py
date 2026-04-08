d59c59b7dda9fe5199f7acbd4fba7c61221e2368
FeliniSy <ninikheladze9@gmail.com>
2026-04-08 20:03:24 +0400

    feat: add variables for gcp

--- utils/settings.py
+++ utils/settings.py
@@ -3,12 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_COIN_MARKET = os.getenv("API_COIN_MARKET")
 COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY")
-API_WITH_ID = os.getenv("API_WITH_ID")
 GCS_BUCKET = os.getenv("GCS_BUCKET")
 BQ_DATASET = os.getenv("BQ_DATASET")
 PROJECT_ID = os.getenv("PROJECT_ID")
 BQ_STAGING = os.getenv("BQ_STAGING")
 BQ_FINAL = os.getenv("BQ_FINAL")
 BASE_URL = os.getenv("BASE_URL")
+
+
+#AirFlow variables
+VS_CURRENCY = "usd"
+DAYS = 90
+GCS_BASE_PATH = "crypto/raw"
+PERE_PAGE = 250
