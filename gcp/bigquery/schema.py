54b005e35014033cb95966905155898c6657d6d2
FeliniSy <ninikheladze9@gmail.com>
2026-04-08 20:05:48 +0400

    feat: create json_schema

--- gcp/bigquery/schema.py
+++ gcp/bigquery/schema.py
@@ -5,14 +5,3 @@ json_schema = [
     bigquery.SchemaField("source_file", "STRING"),
     bigquery.SchemaField("ingested_at", "TIMESTAMP", mode='REQUIRED')
 ]
-
-schema = [
-    bigquery.SchemaField("coin_id", "STRING", mode="REQUIRED"),
-    bigquery.SchemaField("symbol", "STRING"),
-    bigquery.SchemaField("name", "STRING"),
-    bigquery.SchemaField("price", "FLOAT64", mode="REQUIRED"),
-    bigquery.SchemaField("market_cap", "FLOAT64"),
-    bigquery.SchemaField("volume", "FLOAT64"),
-    bigquery.SchemaField("timestamp", "TIMESTAMP", mode="REQUIRED"),
-    bigquery.SchemaField("source_file", "STRING"),
-]
